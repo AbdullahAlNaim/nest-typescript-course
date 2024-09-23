a5a1b6d54199d4af9593379cc5bb91a84c9dc4ab
AbdullahAlNaim <aanaim96@gmail.com>
2024-09-22 21:00:19 -0400

    learning progress

---
+++ maps/dist/index.4680828f.js
@@ -0,0 +1,5 @@
+// import { User } from './User';
+// const user = new User();
+// console.log(user);
+
+//# sourceMappingURL=index.4680828f.js.map
---
+++ maps/src/index.ts
@@ -0,0 +1,9 @@
+import { User } from './User';
+import { Company } from './Company'
+import { google } from '../node_modules/googlemaps'
+
+
+
+new google.maps.Map(document.querySelector('#map'), {
+  zoom
+});
